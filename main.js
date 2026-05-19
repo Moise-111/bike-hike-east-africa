d77fcb7c1f4eb41e04d93a5549350f43f30fd893
Moise-111 <nahimanamoise29@gmail.com>
2026-05-19 17:35:11 +0200

    Update main.js

--- main.js
+++ main.js
@@ -37,13 +37,24 @@ const openBtn = document.getElementById('openBooking');
 const modal = document.getElementById('bookingModal');
 const closeBtn = document.getElementById('closeBooking');
 
-openBtn.addEventListener('click', () => modal.style.display = 'flex');
-closeBtn.addEventListener('click', () => modal.style.display = 'none');
+if (openBtn && modal && closeBtn) {
 
-// Close modal if clicked outside content
-window.addEventListener('click', (e) => {
-    if(e.target === modal) modal.style.display = 'none';
-});
+    openBtn.addEventListener('click', () => {
+        modal.style.display = 'flex';
+    });
+
+    closeBtn.addEventListener('click', () => {
+        modal.style.display = 'none';
+    });
+
+    // Close modal if clicked outside content
+    window.addEventListener('click', (e) => {
+        if (e.target === modal) {
+            modal.style.display = 'none';
+        }
+    });
+
+}
 
 
 document.querySelectorAll(".faq-question").forEach(item => {
